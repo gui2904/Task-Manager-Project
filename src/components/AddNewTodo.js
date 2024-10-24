09620176ab6e297b0cec3ee4142e39675ca2727d
gui2904 <gfulco4@gmail.com>
2024-10-24 15:06:59 -0400

    adding Modal

--- src/components/AddNewTodo.js
+++ src/components/AddNewTodo.js
@@ -1,6 +1,10 @@
 import React, {useState} from 'react'
 import Modal from './Modal.js'
 import { Bell, CalendarDay, Clock, Palette, X } from 'react-bootstrap-icons'
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns'
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import { TimePicker } from '@mui/x-date-pickers/TimePicker';
 
 function AddNewTodo() {
   const [showModal, setShowModal] = useState(false)
@@ -16,6 +20,7 @@ function AddNewTodo() {
         </button>
       </div>
       <Modal showModal={showModal} setShowModal={setShowModal}>
+        <LocalizationProvider dateAdapter={AdapterDateFns}>
         <form>
           <div className="text">
             <h3>Add New TODO!</h3>
@@ -36,14 +41,20 @@ function AddNewTodo() {
               <CalendarDay />
               <p>Choose a day</p>
             </div>
-            date-picker
+            <DatePicker
+              value={day}
+              onChange={day => setDay(day)}
+            />
           </div>
           <div className="pick-time">
             <div className="title">
               <Clock />
               <p>Choose a time</p>
             </div>
-            time-picker
+            <TimePicker
+              value={time}
+              onChange={time => setTime(time)}
+            />
           </div>
           <div className="pick-project">
             <div className="title">
@@ -60,12 +71,13 @@ function AddNewTodo() {
             </div>
           </div>
           <div className="cancel" onClick={() => setShowModal(false)}>
-            <X size='40' style={{ cursor: 'pointer' }} />
+            <X size='40' />
           </div>
           <div className="confirm">
             <button>+ Add TODO</button>
           </div>
         </form>
+        </LocalizationProvider>
       </Modal>
     </div>
   );
