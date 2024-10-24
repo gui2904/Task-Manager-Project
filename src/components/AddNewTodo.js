69bb9edfab7f78327e4ffa15215a4adf81e1d824
gui2904 <gfulco4@gmail.com>
2024-10-24 12:47:04 -0400

    fixes

--- src/components/AddNewTodo.js
+++ src/components/AddNewTodo.js
@@ -1,8 +1,12 @@
 import React, {useState} from 'react'
 import Modal from './Modal.js'
+import { Bell, CalendarDay, Clock, Palette, X } from 'react-bootstrap-icons'
 
 function AddNewTodo() {
   const [showModal, setShowModal] = useState(false)
+  const [text, setText] = useState('')
+  const [day, setDay] = useState(new Date())
+  const [time, setTime] = useState(new Date())
 
   return (
     <div className='AddNewTodo'>
@@ -12,12 +16,56 @@ function AddNewTodo() {
         </button>
       </div>
       <Modal showModal={showModal} setShowModal={setShowModal}>
-        <div>
-          Hello World
-          <button onClick={() => setShowModal(false)}>
-            hide
-          </button>
-        </div>
+        <form>
+          <div className="text">
+            <h3>Add New TODO!</h3>
+            <input
+              type='text'
+              value={text}
+              onChange={e => setText(e.target.value)}
+              placeholder='Todo...'
+              autoFocus
+            />
+          </div>
+          <div className="remind">
+            <Bell />
+            <p>Remind Me!</p>
+          </div>
+          <div className="pick-day">
+            <div className="title">
+              <CalendarDay />
+              <p>Choose a day</p>
+            </div>
+            date-picker
+          </div>
+          <div className="pick-time">
+            <div className="title">
+              <Clock />
+              <p>Choose a time</p>
+            </div>
+            time-picker
+          </div>
+          <div className="pick-project">
+            <div className="title">
+              <Palette />
+              <p>Choose a project</p>
+            </div>
+            <div className="projects">
+              <div className="project active">
+                Personal
+              </div>
+              <div className="project">
+                Work
+              </div>
+            </div>
+          </div>
+          <div className="cancel" onClick={() => setShowModal(false)}>
+            <X size='40' style={{ cursor: 'pointer' }} />
+          </div>
+          <div className="confirm">
+            <button>+ Add TODO</button>
+          </div>
+        </form>
       </Modal>
     </div>
   );
