a8425e5f5ed886bce30f56146546b445aac79ce6
gui2904 <gfulco4@gmail.com>
2024-10-27 21:19:51 -0400

    filtering todos

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react'
 import firebase from '../firebase/'
+import moment from 'moment'
 
 export function useTodos() {
   const [todos, setTodos] = useState([])
@@ -22,6 +23,36 @@ export function useTodos() {
   }, [])
 
   return todos
+}
+
+export function useFilterTodos(todos, selectedProject){
+  const [filterTodos, setFilterTodos] = useState([])
+
+  useEffect(() => {
+    let data;
+    const todayDateFormated = moment().format('MM/DD/YYYY')
+
+    if(selectedProject === 'today'){
+      data = todos.filter(todo => todo.date === todayDateFormated)
+    }else if(selectedProject === 'next 7 days'){
+      data = todos.filter(todo => {
+        const todoDate = moment(todo.date, 'MM/DD/YYYY')
+        const todayDate = moment(todo.date, 'MM/DD/YYYY')
+
+        const diffDays = todoDate.diff(todayDate, 'days')
+
+        return diffDays >= 0 && diffDays < 7
+      })
+    }else if(selectedProject === 'all days'){
+      data = todos
+    }else{
+      data = todos.filter(todo => todo.projectName === selectedProject)
+    }
+
+    setFilterTodos(data)
+  }, [todos, selectedProject])
+
+  return filterTodos
 }
 
 export function useProjects(todos) {
